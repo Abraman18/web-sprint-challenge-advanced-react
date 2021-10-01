3cd8f79ca04d526473d490df3fd60b78ab6cae5f
Abraman18 <abraman18@gmail.com>
2021-10-01 13:08:12 -0700

    finished with custom hook

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import PlantList from "./components/PlantList";
 import ShoppingCart from "./components/ShoppingCart";
 import CheckoutForm from "./components/CheckoutForm";
 
-
 import "./App.css";
 
 function App() {
