1af45977aac04fb52aeaa6960fcc63b5f5bb67a6
Abraman18 <abraman18@gmail.com>
2021-10-01 12:39:25 -0700

    got plantlist to work

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+
+
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
   state = {
@@ -9,6 +11,13 @@ export default class PlantList extends Component {
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+
+  async componentDidMount() {
+    await axios.get('http://localhost:3333/plants').then(response => {
+      this.state.plants= response.data })
+    }
+  
+ 
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
